0e84e8231c5e1d232e3fc2b26b29095d048c7fa8
Rushab Munot <rushabmunot1@gmail.com>
2026-04-03 16:12:55 -0400

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.py
@@ -0,0 +1,29 @@
+import heapq
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        heap = [(0, k)]
+        heapq.heapify(heap)
+
+        graph = {i: [] for i in range(n + 1)}
+        for (u, v, t) in times:
+            graph[u].append((v, t))
+
+        visited = set()
+        lv = 0
+
+        while heap:
+            # first time you pop a node is guranteed to be shortest path
+            time, node = heapq.heappop(heap)
+            visited.add(node)
+            lv += 1
+
+            if lv == n:
+                return time
+
+            for (child, traverse_time) in graph[node]:
+                t = time + traverse_time
+
+                if child not in visited:
+                    heapq.heappush(heap, (t, child))
+
+        return -1
