32f7c65e953511adca56f9b696da7c26f716b724
Rushab Munot <rushabmunot1@gmail.com>
2026-05-16 10:02:13 -0400

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        ret = []
+        for s in strs:
+            ret += [f"{len(s)}"]
+
+        return ",".join(ret) + "$" + "".join(strs)
+
+    def decode(self, s: str) -> List[str]:
+        counts = s.split("$",1)
+        s = counts[1]
+        counts = counts[0].split(",")
+
+        if counts == "":
+            return []
+        ret = []
+        i = 0
+        for n in counts:
+            n = int(n)
+            ret += [s[i:i + n]]
+            i += n
+        return ret
+
